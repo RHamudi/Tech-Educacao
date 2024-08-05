98758e3ca4a76a3dc9d7055c51c944f2d1c45b1a
RHamudi <ramonramos.silva16@gmail.com>
2024-08-05 14:21:44 -0300

    feat: adding routes in project

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css';
 import App from './App.tsx'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import AgendarHorario from './Page/AgendarHorario/index.tsx';
+import Header from './Components/Header/index.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Header />
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/agendar' element={<AgendarHorario />} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
 )
