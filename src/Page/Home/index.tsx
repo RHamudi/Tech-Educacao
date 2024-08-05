ebfbd9edac74102e0dc2448538a8573714ad4cbc
RHamudi <ramonramos.silva16@gmail.com>
2024-08-05 14:23:56 -0300

    feat: adding main in page home

--- src/Page/Home/index.tsx
+++ src/Page/Home/index.tsx
@@ -1,8 +1,8 @@
-import Header from '../../Components/Header'
+import Main from '../../Components/Main';
 function Home() {
     return (
         <>
-            <Header />
+            <Main />
         </> 
     )
 }
