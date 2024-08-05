e003c7d64727b6050d6dd0658a01ccf792dc5f07
RHamudi <ramonramos.silva16@gmail.com>
2024-08-05 14:22:10 -0300

    feat: styles in component home

---
+++ src/Components/Main/index.tsx
@@ -0,0 +1,23 @@
+import background from '../../assets/background-1.png';
+
+function Main(){
+    const backgroundStyle = {
+        backgroundImage: `url(${background})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        height: '100vh',
+        width: '100%',
+      };
+
+    return (
+        <section>
+            <div className='flex flex-col justify-center items-center gap-8' style={backgroundStyle}>
+                <h1 className='text-white text-center font-extrabold text-7xl'>Transformando o seu visual com estilo e precisão.</h1>
+                <h2 className='text-white font-semibold text-xl'>Hórario de funcionamento: 07:00 às 20:00</h2>
+                <button className="p-4 rounded text-black bg-yellow-300">Agende um horario</button>
+            </div>
+        </section>
+    )
+}
+
+export default Main;
